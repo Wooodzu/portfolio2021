6f8d64867cf443977ff3d83397b8215a7a076530
Artur Jasinski <artur199933@gmail.com>
2021-06-06 14:03:18 +0100

    21th push

--- django_project/vendor/urls.py
+++ django_project/vendor/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('vendor-admin/', views.vendor_admin, name='vendor_admin'),
     path('add-product/', views.add_product, name='add_product'),
     path('edit-vendor/', views.edit_vendor, name='edit_vendor'),
-    path('logout/', auth_views.LogoutView.as_view(), name='loggout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='vendor/logout.html'), name='loggout'),
     path('login/', auth_views.LoginView.as_view(template_name='vendor/login.html'), name='loggin'),
     path('', views.vendors, name='vendors'),
     path('<int:vendor_id>/', views.vendor, name='vendor'),
